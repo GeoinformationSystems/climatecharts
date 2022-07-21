547c0ca9d5d9863d8d0121b75a7f6bd1ac8dbbd4
pthiemt <peggy.thiemt@tu-dresden.de>
2022-07-21 15:28:41 +0200

    fix cookie consent and minor adjustments to new versions of datasets

--- source/modules/main.js
+++ source/modules/main.js
@@ -51,15 +51,17 @@ function showTabContent(e, currentTab){
 }
 
 // Manage visibilty of 'Cookie Information Box'
-function setCookieAndHideDiv( accept ){
+function setCookieAndHideDiv(accept){
   if(accept) {
     _paq.push(['rememberCookieConsentGiven']);
-    document.cookie = 'cookie_declined=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+    setCookie('cookie_declined', '', 365);
+	  setCookie('mtm_cookie_consent', 'true', 365);
     $('label[for=optout] strong').text('You are currently accepting tracking cookies. Click here to opt out.');
   }
   else {
     _paq.push(['forgetCookieConsentGiven']);
     setCookie('cookie_declined','true',365);
+    setCookie('mtm_cookie_consent','',365);
     $('label[for=optout] strong').text('You are currently not accepting tracking cookies. Click here to opt in.');
   }
   $("#optout").prop("checked", accept);
